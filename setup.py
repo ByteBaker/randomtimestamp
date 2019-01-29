b2ed0dbcd0ebffd23bea5d05fbcab2f34d47a03b
ByteBaker <42913098+ByteBaker@users.noreply.github.com>
2019-01-30 03:19:57 +0530

    Final upload

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="randomtimestamp",
-    version="1.0.2",
+    version="1.0.0",
     description="Generate random time stamps",
     long_description=README,
     long_description_content_type="text/markdown",
